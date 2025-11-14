62cced7b164f98edf22883c532e839f7c640f952
Salvatore Triunfo <sasi684@hotmail.com>
2025-11-14 20:22:38 +0100

    Initial commit

---
+++ Assets/_Project/MoveSquare.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSquare : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Rigidbody2D _rb;
+    void Start()
+    {
+        if(_rb == null)
+            _rb = GetComponent<Rigidbody2D>();
+        if (_rb == null)
+            Debug.LogError("Nessun componente RigidBody2D trovato!");
+    }
+
+    void FixedUpdate()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2(xInput, yInput);
+        _rb.MovePosition(_rb.position + direction * (_speed * Time.deltaTime));
+    }
+}
